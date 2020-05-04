8349135b36e345c1c6e20db5068c95fb1200e49b
desqui92 <desqui92@gmail.com>
2020-05-04 04:59:50 -0300

    l

---
+++ Vista/js/codigojs.js
@@ -0,0 +1,8 @@
+function colorfondo(){
+    document.getElementsByClassName("colorf")[0].style.backgroundColor="red";
+}
+
+function colornormal(){
+    document.getElementsByClassName("colorf")[0].style.backgroundColor="rgb(8, 226, 19)";
+
+}
